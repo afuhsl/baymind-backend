894f70a80badf738bc8c42d1ab53e510c34a4976
Daniel Lopez <dani.lopez.zermeno@gmail.com>
2024-11-26 18:35:10 -0600

    modified:   src/rutas/auth.js

--- src/rutas/auth.js
+++ src/rutas/auth.js
@@ -87,7 +87,8 @@ router.post('/login', async (req, res) => {
                 user: {
                     id: user._id,
                     name: user.name,
-                    email: user.email
+                    email: user.email,
+                    password: user.password
                 }
             }
         });
